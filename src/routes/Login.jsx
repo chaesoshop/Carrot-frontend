338562a046462215702452b28dedd3ca66a3aadf
chaesoshop <skuspotlight24@gmail.com>
2023-04-28 16:57:03 +0900

    230428

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -160,7 +160,7 @@ const Login = () => {
               }}
               onClick={() => {
                 if (idValue == "") {
-                  window.alert("ID를 입력해 주세요");
+                  window.alert("아이디를 입력해 주세요");
                 } else if (pwValue == "") {
                   window.alert("비밀번호를 입력해 주세요");
                 } else {
